00076b9d3cdd3df7a95744a7302be52a8e54aee1
= <shaul.chamoula@mail.huji.ac.il>
2021-01-17 23:01:33 +0200

    Parcelize all data classes GiphyResponse.kt

--- app/src/main/java/com/github/chiefchiefon/goofigiffy/model/network/GiphyResponse.kt
+++ app/src/main/java/com/github/chiefchiefon/goofigiffy/model/network/GiphyResponse.kt
@@ -1,19 +1,40 @@
 package com.github.chiefchiefon.goofigiffy.model.network
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class GiphyResponse(val data: List<Data>)
 
+/**
+ * kotlinx documentation:
+ * Instructs the Kotlin compiler to generate `writeToParcel()`,
+ * `describeContents()` [android.os.Parcelable] methods,
+ * as well as a `CREATOR` factory class automatically.
+ *
+ * The annotation is applicable only to classes that implements [android.os.Parcelable] (directly or indirectly).
+ * Note that only the primary constructor properties will be serialized.
+ */
+@Parcelize
 @Serializable
 data class Data(
     val id: String,
     val images: Image,
     val title: String
-)
+): Parcelable
+/**
+ * JAVA documentation:
+ * Interface for classes whose instances can be written to
+ * and restored from a {@link Parcel}.  Classes implementing the Parcelable
+ * interface must also have a non-null static field called <code>CREATOR</code>
+ * of a type that implements the {@link Parcelable.Creator} interface.
+ */
 
+@Parcelize
 @Serializable
-data class Image(val downsized_medium: ImageProperties)
+data class Image(val downsized_medium: ImageProperties): Parcelable
 
+@Parcelize
 @Serializable
-data class ImageProperties(val url:String)
+data class ImageProperties(val url:String): Parcelable
